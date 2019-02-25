9bdee7c1862090508060d9b6d6a717ca61801536
idrinkandmakethings <bartender@idrinkandmakethings.com>
2019-02-25 14:27:50 -0800

    Add heading method

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,17 @@ Mahony filter;
 void setup() {
   Serial.begin(9600);
   imu.begin();
-  filter.begin(100);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+
+int getHeading(const int center, const int reading)
+{
+	const int modDiff = (reading - center) % 360;
+	int distance = 180 - abs(abs(modDiff) - 180);
+	return (modDiff + 360) % 360 < 180 ? distance : distance *= -1;
 }
